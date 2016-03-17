b4412e7ad0847a72a6cceead984392747b34d082
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-03-17 15:19:16 +0100

    roman refactored

--- roman.hs
+++ roman.hs
@@ -1,17 +1,10 @@
-romanC :: Char -> Int
-romanC c
-  | c == 'I' = 1
-  | c == 'V' = 5
-  | c == 'X' = 10
-  | c == 'L' = 50
-  | c == 'C' = 100
-  | c == 'D' = 500
-  | c == 'M' = 1000
+import qualified Data.Map as M
+
+c2r = M.fromList [('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',1000)]
 
 f :: Int -> (Int, Int) -> (Int, Int)
 f curr (last, acc) =
   (curr, if curr < last then acc - curr else acc + curr)
 
 roman :: String -> Int
-roman s = snd $ foldr (f) (0,0) xs
-  where xs = map romanC s
+roman s = snd $ foldr (f) (0,0) $ map (c2r M.!) s
